0951b53daba05fc895a9f379ae90ea3653400b57
juliusgutierrez <use.julius.gutierrez@gmail.com>
2025-02-21 14:17:57 +0800

    new ui

--- next.config.ts
+++
@@ -1,9 +0,0 @@
-import type { NextConfig } from "next";
-
-const nextConfig: NextConfig = {
-  reactStrictMode: true, // Enables React strict mode
-  swcMinify: true, // Uses SWC for faster minification
-  output: "export", // Enables static export for GitHub Pages or Netlify
-};
-
-export default nextConfig;
